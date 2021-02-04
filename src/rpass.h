849c1befc21436341ba47e245cd1c71a05534124
Nicholas Schweitzer <f3bl@gmx.com>
2021-02-04 17:02:25 +0100

    Add multisampled-w-depth render pass creator

--- src/rpass.h
+++ src/rpass.h
@@ -46,4 +46,81 @@ void rpass_basic(VkDevice device, VkFormat format, VkRenderPass* rpass) {
         assert(res == VK_SUCCESS);
 }
 
+void rpass_color_depth_multi(VkDevice device, VkFormat color_fmt, VkFormat depth_fmt,
+                             VkSampleCountFlagBits sample_ct, VkRenderPass* rpass)
+{
+        VkAttachmentDescription attachments[3] = {0};
+        // Multisampled color
+        attachments[0].format = color_fmt;
+        attachments[0].samples = sample_ct;
+        attachments[0].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        attachments[0].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        attachments[0].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        attachments[0].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        attachments[0].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        attachments[0].finalLayout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	// Multisampled depth
+        attachments[1].format = depth_fmt;
+        attachments[1].samples = sample_ct;
+        attachments[1].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        attachments[1].storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        attachments[1].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        attachments[1].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        attachments[1].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        attachments[1].finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+        // Color resolve
+        attachments[2].format = color_fmt;
+        attachments[2].samples = VK_SAMPLE_COUNT_1_BIT;
+        attachments[2].loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        attachments[2].storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        attachments[2].stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        attachments[2].stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        attachments[2].initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        attachments[2].finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        VkAttachmentReference color_multisampled_ref = {0};
+        color_multisampled_ref.attachment = 0;
+        color_multisampled_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+        VkAttachmentReference depth_ref = {0};
+        depth_ref.attachment = 1;
+        depth_ref.layout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
+
+        VkAttachmentReference color_resolve_ref = {0};
+        color_resolve_ref.attachment = 2;
+        color_resolve_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+        VkSubpassDescription subpass = {0};
+        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+        subpass.pColorAttachments = &color_multisampled_ref;
+        subpass.pDepthStencilAttachment = &depth_ref;
+        subpass.pResolveAttachments = &color_resolve_ref;
+
+        VkSubpassDependency subpass_dep = {0};
+        subpass_dep.srcSubpass = VK_SUBPASS_EXTERNAL;
+        subpass_dep.dstSubpass = 0;
+        subpass_dep.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
+        	                 | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+        subpass_dep.srcAccessMask = 0;
+        subpass_dep.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT
+        	                 | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
+        subpass_dep.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT
+                                  | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+
+        VkRenderPassCreateInfo info = {0};
+        info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+        info.attachmentCount = sizeof(attachments) / sizeof(attachments[0]);
+        info.pAttachments = attachments;
+        info.subpassCount = 1;
+        info.pSubpasses = &subpass;
+        info.dependencyCount = 1;
+        info.pDependencies = &subpass_dep;
+
+        VkResult res = vkCreateRenderPass(device, &info, NULL, rpass);
+        assert(res == VK_SUCCESS);
+}
+
 #endif // LL_RPASS_H
